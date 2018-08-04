d3cab98a835dd5c32758a2e022631e19034662d9
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2018-08-04 22:16:39 +0800

    新增modal元件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <SideBar/>
     <MenuBar/>
     <Calendar/>
+    <Modal :show="showModal" :pkg="modalPKG"/>
   </div>
 </template>
 
@@ -10,13 +11,25 @@
 import SideBar from "./components/SideBar.vue";
 import MenuBar from "./components/MenuBar.vue";
 import Calendar from "./components/Calendar.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "app",
   components: {
     SideBar,
     MenuBar,
-    Calendar
+    Calendar,
+    Modal
+  },
+  data: function() {
+    return {
+      showModal: false,
+      modalPKG: {
+        header: "HEADER TEXT",
+        body: "BODY TEXT",
+        footer: "FOOTER TEXT"
+      }
+    };
   }
 };
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="main" v-show="show" >
+      <div v-show="show" class="modalMain">
+          <div class="modalHeader">
+            {{pkg.header}}
+          </div>
+          <div class="modalBody">
+            {{pkg.body}}
+          </div>
+          <div class="modalFooter">
+            {{pkg.footer}}
+          </div>
+      </div>
+      <div class="modalBG"></div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    show: { type: Boolean, dafault: true },
+    pkg: {
+      type: Object,
+      default: function() {
+        return {
+          header: "hhh",
+          body: "bbb",
+          footer: "fff"
+        };
+      }
+    }
+  },
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+* {
+  margin: 0px;
+}
+.main {
+  width: 100%;
+  height: 100%;
+}
+.modalBG {
+  z-index: 1400;
+  width: 100%;
+  height: 100%;
+  background: #00000038;
+  position: fixed;
+}
+
+.modalMain {
+  cursor: pointer;
+  z-index: 1401;
+  position: absolute;
+  margin: 0px auto;
+  width: 500px;
+  top: 20%;
+  left: calc(50% - 250px);
+}
+.modalHeader {
+  background: #74b3ce;
+  height: 20px;
+}
+.modalBody {
+  background: #508991;
+  height: 300px;
+}
+.modalFooter {
+  background: #09bc8a;
+  height: 20px;
+}
+</style>
